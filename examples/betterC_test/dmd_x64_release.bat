0586ca004061e74ea0898812f2d55298afcb93a6
dokutoku <3729541-dokutoku@users.noreply.gitlab.com>
2019-10-09 18:00:14 +0900

    fix 75a4ceb5632a54b754e33a21c5d216f95b9c0cb6

--- examples/betterC_test/dmd_x64_release.bat
+++ examples/betterC_test/dmd_x64_release.bat
@@ -1,5 +1,5 @@
 call msvcEnv.bat x64
 "%HOMEDRIVE%\D\dmd2\windows\bin\dub.exe" build --compiler=dmd --arch=x86_64 --force --combined --build=release_sample --config=betterC_test1 --override-config=npp-api/betterC_library
 "%HOMEDRIVE%\D\dmd2\windows\bin\dub.exe" build --compiler=dmd --arch=x86_64 --force --combined --build=release_sample --config=betterC_test2 --override-config=npp-api/betterC_library
-move /Y ".\plugins\betterC_test1\betterC_test1.dll" ".\plugins\betterC_test1\dmd_betterC_test1_x64.dll"
-move /Y ".\plugins\betterC_test2\betterC_test2.dll" ".\plugins\betterC_test2\dmd_betterC_test2_x64.dll"
+move /Y ".\plugins\betterC-test1\betterC-test1.dll" ".\plugins\betterC-test1\dmd_betterC-test1_x64.dll"
+move /Y ".\plugins\betterC-test2\betterC-test2.dll" ".\plugins\betterC-test2\dmd_betterC-test2_x64.dll"
--- examples/betterC_test/ldc_x86_release.bat
+++ examples/betterC_test/ldc_x86_release.bat
@@ -1,5 +1,5 @@
 call msvcEnv.bat
 "%HOMEDRIVE%\ldc2\bin\dub.exe" build --compiler=ldc2 --arch=x86 --force --combined --build=release_sample --config=betterC_test1 --override-config=npp-api/betterC_library
 "%HOMEDRIVE%\ldc2\bin\dub.exe" build --compiler=ldc2 --arch=x86 --force --combined --build=release_sample --config=betterC_test2 --override-config=npp-api/betterC_library
-move /Y ".\plugins\betterC_test1\betterC_test1.dll" ".\plugins\betterC_test1\ldc_betterC_test1_x86.dll"
-move /Y ".\plugins\betterC_test2\betterC_test2.dll" ".\plugins\betterC_test2\ldc_betterC_test2_x86.dll"
+move /Y ".\plugins\betterC-test1\betterC-test1.dll" ".\plugins\betterC-test1\ldc_betterC-test1_x86.dll"
+move /Y ".\plugins\betterC-test2\betterC-test2.dll" ".\plugins\betterC-test2\ldc_betterC-test2_x86.dll"
